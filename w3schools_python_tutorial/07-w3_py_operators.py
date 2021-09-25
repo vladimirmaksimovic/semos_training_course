d1754bdbd3acdc144fba0f38e056d90c162df4aa
VM <vmaksimovic.cr34@gmail.com>
2021-09-25 23:23:21 +0200

    python operators

---
+++ w3schools_python_tutorial/07-w3_py_operators.py
@@ -0,0 +1,66 @@
+# Operators
+
+a, b = [2, 3]
+
+# Arithmetic Operators
+
+a + b  # Addition
+a - b  # Substraction
+a * b  # Multiplication
+a / b  # Division
+a % b  # Modulus
+a ** b  # Exponentiation
+a // b  # Floor division
+
+# Assignment Operators
+
+a = 5     # a = 5
+a += 3    # a = a + 3
+a -= 3    # a = a - 3
+a *= 3    # a = a * 3
+a /= 3    # a = a / 3
+a %= 3    # a = a % 3
+a //= 3   # a = a // 3
+a **= 3   # a = a ** 3
+a &= 3    # a = a & 3
+a |= 3    # a = a | 3
+a ^= 3    # a = a ^ 3
+a >>= 3   # a = a >> 3
+a <<= 3   # a = a << 3
+
+# Comparison Operators
+
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+
+# Logical Operators
+
+a < 5 and a < 10        # and
+a < 5 or a < 10         # or
+not(a < 5 and a < 10)   # not
+
+# Indentity Operators
+
+a is b      # True/False
+a is not b  # True/False
+
+# Membership Operators
+
+a in b      # True/False
+a not in b  # True/False
+
+# Bitwise Operators
+
+
+"""
+&   # AND
+|   # OR
+^   # XOR
+~   # NOT
+<<  # Zero fill left shift
+>>  # Signed right shift
+"""
