b24fbf8a3a6324b0eb107c420cf0c218a62b5797
vladimirmaksimovic <vmaksimovic.cr34@gmail.com>
2021-11-10 09:59:32 +0100

    python imports and modules

---
+++ moodle_tutorials/python/python-03.py
@@ -0,0 +1,10 @@
+""" Importing and using modules """
+
+# import math module
+import math
+
+# use math module methods
+print("The square root of 16 is => ", math.sqrt(16))
+
+# use math module constant
+print("Pi is: ", math.pi)
